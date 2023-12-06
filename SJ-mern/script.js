3b24877be8f80f737e13e6019a3b03c27127dba2
sooraj6521 <145972447+sooraj6521@users.noreply.github.com>
2023-12-06 11:31:16 +0530

    Add files via upload

--- SJ-mern/script.js
+++ SJ-mern/script.js
@@ -1,73 +1,121 @@
-{
-console.log("hello javscript");
+
+// console.log("hello javscript");
 
 
-let obj ={
-    name:"harry",
-    age :30,
-    greeting : function () {
-        console.log("name :",this.name);
+// let obj ={
+//     name:"harry",
+//     age :30,
+//     greeting : function () {
+//         console.log("name :",this.name);
+//         console.log("age :",this.age);
+//     }
+// }
+// console.log("obj :",obj);
+
+// obj.greeting();
+
+
+// //constructor function
+// function person (name,age,mark)
+
+
+// const person1 = new person ("john",30);
+// console.log("person1 :",person1);
+// person1.greeting();
+
+// const person2 = new person ("mahe",40);
+// console.log("person2 :",person2);
+// person2.greeting();
+
+// //updating individual objectes
+// person2.place ="ERKLM"
+// console.log("person2.place :", person2.place);
+
+
+// person.prototype.getMark = function (){
+//     return this.mark;
+// }
+// console.log("mark of the person1 :",person1.getMark());
+// console.log("mark of the person2 :",person2.getMark());
+// }
+
+
+// {
+
+// let obj ={
+//     name:"volkswagan",
+//     colour :"red",
+//     greeting :function(){
+//         console.log("name :",this.name);
+//         console.log("colour :",this.colour);
+//     }
+// }
+// console.log("obj :",obj);
+
+// obj.greeting();
+
+
+
+// function car(name,colour){
+//     this.name ="mazda",
+//     this.colour ="black"
+//     this.greeting = function(){
+//         console.log("name :",this.name);
+//         console.log("age :",this.colour);
+//     }
+
+// }
+// const car1 = new car ("mercedez","black");
+// console.log("car1 :",car1);
+// car1.greeting();
+
+// const car2 = new car ("nizzan","blue");
+// console.log("car2 :",car2);
+// car2.greeting();
+// }
+
+//using classes
+class person {
+    name;
+    age;
+    mark;
+
+    constructor(name,age,mark){
+        this.name = name;
+        this.age=age;
+        this.mark=mark;
+    }
+    greeting(){
+        console.log("name : ",this.name);
         console.log("age :",this.age);
-    }
-}
-console.log("obj :",obj);
-
-obj.greeting();
-
-
-//constructor function
-function person(name,age){
-    this.name ="harry",
-    this.age =30,
-    this.greeting = function(){
-        console.log("name :",this.name);
-        console.log("age :",this.age);
+        console.log("mark :",this.mark);
     }
 
 }
+class Animal{
+    constructor(name){
+        this.name = name;
 
-
-const person1 = new person ("john",30);
-console.log("person1 :",person1);
-person1.greeting();
-
-const person2 = new person ("mahe",40);
-console.log("person2 :",person2);
-person2.greeting();
-}
-
-
-{
-
-let obj ={
-    name:"volkswagan",
-    colour :"red",
-    greeting :function(){
-        console.log("name :",this.name);
-        console.log("colour :",this.colour);
+    }
+    walk(){
+        console.log("Animal walking...");
     }
 }
-console.log("obj :",obj);
 
-obj.greeting();
-
-
-
-function car(name,colour){
-    this.name ="mazda",
-    this.colour ="black"
-    this.greeting = function(){
-        console.log("name :",this.name);
-        console.log("age :",this.colour);
+class Dog extends Animal{
+    constructor(name,nickName) {
+        super(name);
+        this.nickName = nickName;
     }
-
-}
-const car1 = new car ("mercedez","black");
-console.log("car1 :",car1);
-car1.greeting();
-
-const car2 = new car ("nizzan","blue");
-console.log("car2 :",car2);
-car2.greeting();
+    walk() {
+        console.log("Dog walking...");
+    }
 }
 
+let myAnimal = new Animal ("monkey");
+console.log("myAnimal : ",myAnimal);
+myAnimal.walk();
+
+let myDog = new Dog("parent of arjun","Arjun");
+console.log("myDog :",myDog);
+myDog.walk();
